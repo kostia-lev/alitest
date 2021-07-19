d17646fae83514aa9f0b136539f10cc3088c4080
Konstantinv Vahrushev <kostia_lev@hotmail.com>
2021-07-19 17:12:17 +0300

    rn

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/stonextest'),
+      dir: require('path').join(__dirname, './coverage/alitest'),
       subdir: '.',
       reporters: [
         { type: 'html' },
